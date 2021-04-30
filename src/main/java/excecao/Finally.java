c0ca97ec856c783863fccaa5d23c2bff20ae8b08
Wesley Costa <wesferrcosta@gmail.com>
2021-04-29 22:00:14 -0300

    Finally

---
+++ src/main/java/excecao/Finally.java
@@ -0,0 +1,17 @@
+package excecao;
+
+import java.util.Scanner;
+
+public class Finally {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        try {
+            System.out.println(7 / entrada.nextInt());
+            entrada.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Finalmente...");
+        }
+    }
+}
