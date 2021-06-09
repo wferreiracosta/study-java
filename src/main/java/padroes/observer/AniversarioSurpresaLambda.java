6a5871250e08b784014243d9915c5fe5b1e313ec
Wesley Costa <wesferrcosta@gmail.com>
2021-06-08 23:06:01 -0300

    Padrão Observer

---
+++ src/main/java/padroes/observer/AniversarioSurpresaLambda.java
@@ -0,0 +1,16 @@
+package padroes.observer;
+
+public class AniversarioSurpresaLambda {
+    public static void main(String[] args) {
+        Porteiro porteiro = new Porteiro();
+        Namorada namorada = new Namorada();
+        porteiro.registrarObservador(namorada);
+
+        porteiro.registrarObservador(e -> {
+            System.out.println("Supresa via lambda!");
+            System.out.println("Ocorreu em: "+e.getMomento());
+        });
+
+        porteiro.monitorar();
+    }
+}
