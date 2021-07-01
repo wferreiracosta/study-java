e962dd034fd637213563d41bbd681e28a9498203
Wesley Costa <wesferrcosta@gmail.com>
2021-06-30 22:02:54 -0300

    Fabrica de conexões

--- src/main/java/jdbc/CriarBanco.java
+++ src/main/java/jdbc/CriarBanco.java
@@ -7,11 +7,7 @@ import java.sql.Statement;
 
 public class CriarBanco {
     public static void main(String[] args) throws SQLException {
-        final String URL = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
-        final String USUARIO = "root";
-        final String SENHA = "password";
-
-        Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+        Connection conexao = FabricaConexao.getConexao();
 
         Statement statement = conexao.createStatement();
         statement.execute("CREATE DATABASE IF NOT EXISTS curso_java");
---
+++ src/main/java/jdbc/FabricaConexao.java
@@ -0,0 +1,20 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+    public static Connection getConexao() {
+        final String URL = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
+        final String USUARIO = "root";
+        final String SENHA = "password";
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
