75d8afba530194400c42ab09f517cf01dc04c732
Wesley Costa <wesferrcosta@gmail.com>
2021-07-10 01:35:38 -0300

    Usando properties

--- src/main/java/jdbc/FabricaConexao.java
+++ src/main/java/jdbc/FabricaConexao.java
@@ -1,20 +1,33 @@
 package jdbc;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
     public static Connection getConexao() {
-        final String URL = "jdbc:mysql://localhost:3306/curso_java?verifyServerCertificate=false&useSSL=true";
-        final String USUARIO = "root";
-        final String SENHA = "password";
         try {
+            Properties properties = getProperties();
+
+            final String URL = properties.getProperty("banco.url");
+            final String USUARIO = properties.getProperty("banco.usuario");
+            final String SENHA = properties.getProperty("banco.senha");
+
             return DriverManager.getConnection(URL, USUARIO, SENHA);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        String caminho = "/src/main/resources/conexao.properties";
+        properties.load(FabricaConexao.class.getResourceAsStream(caminho));
+        return properties;
     }
 
 }
